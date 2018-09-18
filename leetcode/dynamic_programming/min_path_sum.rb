400e0e11cca1066bf588827ef9fe2eea7fc27c3e
Nick Calibey <nick.calibey@gmail.com>
2018-09-18 17:19:43 -0500

    fixed edge case and memoized

--- leetcode/dynamic_programming/min_path_sum.rb
+++ leetcode/dynamic_programming/min_path_sum.rb
@@ -1,22 +1,28 @@
 def min_path_sum_helper(grid, row, col, cache)
   return grid[row][col] if row === 0 && col === 0
-  return 0 if col < 0 || row < 0
+  return nil if col < 0 || row < 0
 
   val = grid[row][col]
-  left = min_path_sum_helper(grid, row, col - 1, cache)
-  up = min_path_sum_helper(grid, row - 1, col, cache)
 
-  if left == 0
-    return val + up
-  elsif up == 0
-    return val + left
-  end
+  left = if cache[[row, col - 1]]
+           cache[[row, col - 1]]
+         else
+           min_path_sum_helper(grid, row, col - 1, cache)
+         end
+
+  up = if cache[[row - 1, col]]
+         cache[[row -1, col]]
+       else
+         min_path_sum_helper(grid, row - 1, col, cache)
+       end
+
+  return val + up if left.nil?
+  return val + left if up.nil?
 
   left += val
   up += val
 
-
-  return [left, up].min
+  cache[[row, col]] = [left, up].min
 end
 
 def min_path_sum(grid)
@@ -29,26 +35,26 @@ p min_path_sum([
   [1,3,1],
   [1,5,1],
   [4,2,1]
-  ])
+  ]) == 7
 
 p min_path_sum([
   [1,4,1],
   [1,1,1],
   [4,2,1]
-  ])
+  ]) == 5
 
 p min_path_sum([
   [1,4],
   [2,3],
   [4,2]
-  ])
+  ]) == 8
 
 p min_path_sum([
   [1,4],
   [2,3],
-  ])
+  ]) == 6
 
 p min_path_sum([
   [1,4,5],
   [2,3,6],
-  ])
+  ]) == 12
