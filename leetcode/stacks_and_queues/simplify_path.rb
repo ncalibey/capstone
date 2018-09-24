7a32eceff4cf9cae1948cb73d418bd3f161c07cf
Nick Calibey <nick.calibey@gmail.com>
2018-09-23 21:32:44 -0500

    added working solution for 'Simplify Path'

---
+++ leetcode/stacks_and_queues/simplify_path.rb
@@ -0,0 +1,46 @@
+class Stack
+
+  def initialize
+    @construct = []
+    @top = 0
+  end
+
+  def push(item)
+    @construct.push(item)
+    @top += 1
+  end
+
+  def pop
+    @construct.pop
+    @top -= 1
+  end
+
+  def read
+    @construct.join
+  end
+end
+
+def simplify_path(path)
+  parts = path.scan(/[^\/]+/)
+  stack = Stack.new
+
+  parts.each do |dir|
+    if dir == '..'
+      stack.pop
+    elsif dir != '.'
+      stack.push("/#{dir}")
+    end
+  end
+
+  new_path = stack.read
+
+  new_path == '' ? '/' : new_path
+end
+
+p simplify_path('/../')
+p simplify_path('/home/')
+p simplify_path("/a/./b/../../c/")
+p simplify_path("/a/../../b/../c//.//")
+p simplify_path("/a//b////c/d//././/..")
+p simplify_path("/a/b/c/../../..")
+p simplify_path("/...")
