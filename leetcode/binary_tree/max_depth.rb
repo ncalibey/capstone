954092c13cb2d3e53dd6f73a71a11d292dcfb439
Nick Calibey <nick.calibey@gmail.com>
2018-09-25 22:38:36 -0500

    refactored

--- leetcode/binary_tree/max_depth.rb
+++ leetcode/binary_tree/max_depth.rb
@@ -7,28 +7,12 @@ class TreeNode
   end
 end
 
-def max_tree_helper(node, level=1)
-  return level if node.left.nil? && node.right.nil?
-
-  if node.left.nil?
-    return max_tree_helper(node.right, level + 1)
-  elsif node.right.nil?
-    return max_tree_helper(node.left, level + 1)
-  end
-
-  left, right = node.left, node.right
-  left_result = max_tree_helper(left, level + 1)
-  right_result = max_tree_helper(right, level + 1)
-
-  [left_result, right_result].max
-end
-
 def max_depth(tree)
   return 0 if tree.nil?
 
   left, right = tree.left, tree.right
-  left_result = max_tree_helper(left) + 1
-  right_result = max_tree_helper(right) + 1
+  left_result = max_depth(left) + 1
+  right_result = max_depth(right) + 1
 
   [left_result, right_result].max
 end
